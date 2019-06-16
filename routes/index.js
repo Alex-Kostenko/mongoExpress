f9ca485e8b348059ba9b9d066e14b47eeb149fad
Alex <alex@Mac-mini-Alex.local>
2019-06-16 11:25:47 +0300

    updata db

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://admin:admin@localhost:32781/admin', { useNewUrlParser: true });
+const bodyParser = require("body-parser");
+mongoose.connect('mongodb://admin:admin@localhost:27017/admin', { useNewUrlParser: true });
+var app = express();
+const urlencodedParser = bodyParser.urlencoded({ extended: true });
 
 var carSchema = new mongoose.Schema({
-  name: String,
   carId: Number,
+  user: String,
+  name: String,
+  data: String,
 });
 
 var Cars = mongoose.model('CarsList', carSchema);
@@ -14,6 +19,7 @@ let count = 0;
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  console.log('test0');
   Cars.find({ name: /.+/ }, (error, cars) => {
     res.render('index', { title: 'Express', cars });
   });
@@ -26,12 +32,58 @@ router.get('/comments/:carId', function (req, res, next) {
 });
 
 router.post('/', function (req, res, next) {
-  console.log(req.body.name);
+
+console.log('test1');
 
   var silence = new Cars({ name: req.body.name });
   silence.save((error, el) => {
     res.render('saveSuccess', { name: req.body.name, title: 'Saved!' });
   });
+});
+
+router.get('/test/:comment', function (req, res, next) {
+  var toyota = new Cars({
+    carId: 0,
+    user: 'Toyota',
+    name: req.params.comment,
+  });
+
+  toyota.save((error, el) => {
+    res.json(toyota );
+  });
+});
+
+router.post("/submit", urlencodedParser, function (req, res) {
+  console.log(Cars);
+  console.log('Add new car!!!');
+  
+  if (!req.body) return res.sendStatus(400);
+
+  var newCar = new Cars({
+    carId: req.body.id,
+    user: req.body.userName,
+    name: req.body.comment,
+    data: req.body.data,
+  });
+
+  newCar.save((error, el) => {
+    if (!req.body) return response.sendStatus(400);
+    res.send(`${req.body.id} - ${req.body.userName} - ${req.body.comment} - ${req.body.data}`);
+  });
+
+
+});
+
+router.get("/db", function (req, res) {
+  console.log(CarsList);
+  
+  console.log(JSON.stringify(CarsList));
+  // Card.find().pretify;
+});
+
+app.get("/submit", urlencodedParser, function (req, res) {
+  res.render('index', { title: 'Express', cars });
+  // res.sendFile(__dirname + "/");
 });
 
 module.exports = router;
